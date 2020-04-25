52f996afae976d0b98a61bc02498d5de625b683d
firatatalay34 <firatatalay34@hotmail.com>
2020-04-25 18:05:25 +0300

    İlkCommit

---
+++ OyunProgramlamaVize/Assets/top.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class top : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed = 10f;
+    oyuncu oyuncu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ikisaniye", 2);
+        oyuncu = GameObject.FindObjectOfType<oyuncu>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    void ikisaniye()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        oyuncu.SkorYap();
+    }
+}
